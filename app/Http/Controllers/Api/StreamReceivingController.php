5fd5f2026527d930d3c9dc8cfe60a4528b904cdf
emincanozcan <50255411+emincanozcan@users.noreply.github.com>
2021-12-11 09:15:25 +0300

    Refactor stream receiver hooks

---
+++ app/Http/Controllers/Api/StreamReceivingController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Stream;
+use App\Models\User;
+
+class StreamReceivingController extends Controller
+{
+    public function start()
+    {
+        $streamKey = request()->input('name');
+
+        $user = User::where(['stream_key' => $streamKey])->first();
+
+        if (!($user)) {
+            return response()->json(['status' => false], 402);
+        }
+
+        $stream = $user->streams()->create([
+            'started_at' => now(),
+        ]);
+
+        return response()->redirectTo("rtmp://0.0.0.0:1935/hls_converter/{$stream->id}");
+    }
+
+    public function done()
+    {
+        $streamId = (int) request()->input('name');
+
+        Stream::find($streamId)->update([
+            'ended_at' => now(),
+        ]);
+
+        return response()->json(['status' => 'ok']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\StreamReceivingController;
 use App\Models\Stream;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -22,61 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-
-// RESPONSE FORMAT
-//
-// [2021-12-10 21:15:39] local.INFO: start stream
-// [2021-12-10 21:15:39] local.INFO: array (
-//   'app' => 'stream_receiver',
-//   'flashver' => 'FMLE/3.0 (compatible; FMSc/1.0)',
-//   'swfurl' => 'rtmp://0.0.0.0:1935/stream_receiver',
-//   'tcurl' => 'rtmp://0.0.0.0:1935/stream_receiver',
-//   'pageurl' => NULL,
-//   'addr' => '172.20.0.1',
-//   'clientid' => '1',
-//   'call' => 'publish',
-//   'name' => 'can',
-//   'type' => 'live',
-// )
-// [2021-12-10 21:15:43] local.INFO: done stream
-// [2021-12-10 21:15:43] local.INFO: array (
-//   'app' => 'stream_receiver',
-//   'flashver' => 'FMLE/3.0 (compatible; FMSc/1.0)',
-//   'swfurl' => 'rtmp://0.0.0.0:1935/stream_receiver',
-//   'tcurl' => 'rtmp://0.0.0.0:1935/stream_receiver',
-//   'pageurl' => NULL,
-//   'addr' => '172.20.0.1',
-//   'clientid' => '1',
-//   'call' => 'publish_done',
-//   'name' => 'can',
-// )
-//
-
-Route::post('stream/start', function (Request $request) {
-    $streamKey = $request->input('name');
-
-    $user = User::where(['stream_key' => $streamKey])->first();
-
-    if (!($user)) {
-        return response()->json(['status' => false], 402);
-    }
-
-    $username = $user->username;
-
-    $stream = $user->streams()->create([
-        'started_at' => now(),
-    ]);
-
-    return response()->redirectTo("rtmp://0.0.0.0:1935/hls_converter/{$stream->id}");
-});
-
-
-Route::post('stream/done', function (Request $request) {
-    $streamId = (int) $request->input('name');
-
-    Stream::find($streamId)->update([
-        'ended_at' => now(),
-    ]);
-
-    return response()->json(['status' => 'ok']);
-});
+Route::post('stream/start', [StreamReceivingController::class, 'start']);
+Route::post('stream/done', [StreamReceivingController::class, 'done']);
