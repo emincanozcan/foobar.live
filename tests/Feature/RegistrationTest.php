5c360bb960923d461cf41f42ab4170c6ec5e7c7f
emincanozcan <50255411+emincanozcan@users.noreply.github.com>
2021-12-12 11:02:23 +0300

    Update tests & add github workflow to run tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -42,6 +42,7 @@ class RegistrationTest extends TestCase
 
         $response = $this->post('/register', [
             'name' => 'Test User',
+            'username' => 'iComposeArtLikeTheGhostOfMozart',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
