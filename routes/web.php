a144ea638e1b6a23905fd2d7a624ab89d8674c50
emincanozcan <50255411+emincanozcan@users.noreply.github.com>
2021-12-12 21:58:20 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('u/{username}', function () {
     $user = User::where(['username' => request('username')])->first();
     throw_if($user === null, new ModelNotFoundException());
 
+    // I AM 99.9% SURE THAT THIS IS UNNECESSARY BUT I DO NOT WANT TO REMOVE IT AT THE LAST MINUTES OF A HACKATHON.
     $title = $user->currentLiveStream()?->title ? $user->currentLiveStream()->title : 'Live Stream Of: '.$user->username;
 
     return view('stream.show', [
